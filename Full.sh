803814dcac3606108a35318f46ec5ddc1ccb81c3
Maxyapper7 <65361371+Maxyapper7@users.noreply.github.com>
2022-10-15 16:35:39 -0600

    Update Full.sh

--- Full.sh
+++ Full.sh
@@ -7,6 +7,8 @@ flatpak install flathub -y com.google.Chrome com.obsproject.Studio io.github.shi
 
 dnf install steam lutris godot filezilla -y
 
+dnf remove gnome-boxes gnome-calendar libreoffice-calc libreoffice-impress libreoffice-writer gnome-logs gnome-maps gnome-photos
+
 dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
 dnf groupupdate sound-and-video -y
 
