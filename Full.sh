7e7e5aff0e3683a0a776a142f537edabab292df3
Maxyapper7 <65361371+Maxyapper7@users.noreply.github.com>
2022-10-16 10:35:45 -0600

    Update Full.sh

--- Full.sh
+++ Full.sh
@@ -9,7 +9,8 @@ flatpak install flathub -y com.google.Chrome com.obsproject.Studio io.github.shi
 
 dnf install steam lutris godot filezilla vim -y
 
-dnf remove gnome-boxes gnome-calendar libreoffice-calc libreoffice-impress libreoffice-writer gnome-logs gnome-maps gnome-photos
+#Removes preinstall Gnome stuff
+dnf remove gnome-boxes gnome-calendar libreoffice-calc libreoffice-impress libreoffice-writer gnome-logs gnome-maps gnome-photos gnome-characters gnome-contacts gnome-weather gnome-tour gnome-clocks gnome-font-viewer
 
 dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
 dnf groupupdate sound-and-video -y
